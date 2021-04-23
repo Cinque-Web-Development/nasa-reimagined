8117b7de11cb27bb98aa60c8ad4ec4efa088da6e
kstick9210 <kstick9210@outlook.com>
2021-04-23 11:33:26 -0500

    remove extranaous code and add automerge file

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://www.cinquewd.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit Cinque Web Development
-        </a>
-      </header>
+      NASA Reimagined
     </div>
   );
 }

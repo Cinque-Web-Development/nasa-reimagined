dde441d6570d83ee972d23fab626c70203aa48b8
erikacolette <erikatidwell.c@gmail.com>
2021-04-27 13:23:54 -0400

    links working

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,24 +4,24 @@ import React from "react";
 const Navbar = () => {
   return (
    
-      <div className="ui secondary top fixed menu">
-        <a href="/" className="item">
+      <div id="nav-container" className="ui secondary top fixed menu">
+        <a href="/" id="nasa" className="item">
           NASA
         </a>
         <div className="right menu">
-        <a href="/" className="item">
-        About
+        <a href="https://www.nasa.gov/about/index.html" target="_blank" rel="noopener noreferrer" id="about"className="item">
+    About
         </a>
-        <a href="/" className="item">
+        <a href="https://www.nasa.gov/topics/humans-in-space" target="_blank" rel="noopener noreferrer" className="item">
         <img src="/images/astronaut.png"></img>
         </a>
-        <a href="/" className="item">
+        <a href="https://www.nasa.gov/topics/technology/index.html" target="_blank" rel="noopener noreferrer"  className="item">
         <img src="/images/telescope.png"></img>
         </a>
-        <a href="/" className="item">
+        <a href="https://www.nasa.gov/topics/solarsystem/index.html" target="_blank" rel="noopener noreferrer" className="item">
         <img src="/images/planets.png"></img>
         </a>
-        <a href="/" className="item">
+        <a href="https://www.nasa.gov/stem" className="item" target="_blank" rel="noopener noreferrer" >
         <img src="/images/book.png"></img>
         </a>
         

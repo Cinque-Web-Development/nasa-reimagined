2d650143a2c42cf373cb2c9a84051395fa32a5d8
erikacolette <erikatidwell.c@gmail.com>
2021-04-27 12:51:22 -0400

    navbar icons up

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,26 +4,34 @@ import React from "react";
 const Navbar = () => {
   return (
    
-      <div className="ui secondary  menu">
-        <a href="/" className="active item">
-          Home
-        </a>
+      <div className="ui secondary top fixed menu">
         <a href="/" className="item">
-          Messages
-        </a>
-        <a href="/" className="item">
-          Friends
+          NASA
         </a>
         <div className="right menu">
+        <a href="/" className="item">
+        About
+        </a>
+        <a href="/" className="item">
+        <img src="/images/astronaut.png"></img>
+        </a>
+        <a href="/" className="item">
+        <img src="/images/telescope.png"></img>
+        </a>
+        <a href="/" className="item">
+        <img src="/images/planets.png"></img>
+        </a>
+        <a href="/" className="item">
+        <img src="/images/book.png"></img>
+        </a>
+        
           <div className="item">
             <div className="ui icon input">
               <input type="text" placeholder="Search..." />
               <i className="search link icon"></i>
             </div>
           </div>
-          <a href="/" className="ui item">
-            Logout
-          </a>
+         
         </div>
       </div>
     

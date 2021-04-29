77679193f349172f7778aad6b1ef6c78c4c85c6f
erikacolette <erikatidwell.c@gmail.com>
2021-04-29 12:29:40 -0400

    optional css starry night

--- src/components/sectionOne.js
+++ src/components/sectionOne.js
@@ -4,15 +4,30 @@ import "./sectionOne.css";
 const SectionOne = () => {
   return (
     <div className="background">
-        <div className="moon-ship-box"><div className="moon"></div></div>
-        <div className="ship-box"><div className="ship"></div></div>
-        <div className="earth-box"><div className="earth"></div></div>
+      <div class="stars"></div>
+      <div class="twinkling"></div>
+      <div className="moon-ship-box">
+        <div className="moon"></div>
+      </div>
+      <div className="ship-box">
+        <div className="ship"></div>
+      </div>
+      <div className="earth-box">
+        <div className="earth"></div>
+      </div>
       <div className="text-box">
         <h1>The National Aeronautics and Space Administration </h1>
         <p>
           "Explore, discover, and expand knowledge for the benefit of humanity."
         </p>
-        <a href="https://www.nasa.gov/socialmedia" target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary follow-button">Follow</a>
+        <a
+          href="https://www.nasa.gov/socialmedia"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-outline-primary follow-button"
+        >
+          Follow
+        </a>
       </div>
     </div>
   );

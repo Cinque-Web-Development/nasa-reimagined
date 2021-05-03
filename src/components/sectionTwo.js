88ff08033e5d2358a00a5ee14668dd83a2856231
erikacolette <erikatidwell.c@gmail.com>
2021-05-03 18:23:10 -0400

    jupiter modifications

--- src/components/sectionTwo.js
+++ src/components/sectionTwo.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './sectionTwo.css'
+import React from "react";
+import "./sectionTwo.css";
 
-import APOD from './APOD/APOD'
-
+import APOD from "./APOD/APOD";
 
 const SectionTwo = () => {
-    return ( <>
-    <div className="news-feed">News Feed</div>
-    <APOD/>
-    </> );
-}
- 
+  return (
+    <div className="sectiontwo-background">
+        <div className="jupiter-box">
+            <div className="jupitertop"></div>
+            <div className="jupiterRing"></div>
+            <div className="jupiterBottom"></div>
+        </div>
+       
+      <div className="news-feed">News Feed</div>
+      <APOD />
+    </div>
+  );
+};
+
 export default SectionTwo;

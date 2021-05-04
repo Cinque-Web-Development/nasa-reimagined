5ff52a00f5d7a92202b1e947f7ccbe0272dc4846
erikacolette <erikatidwell.c@gmail.com>
2021-05-04 13:20:56 -0400

    z-index and hid jupitar and section three

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/navbar'
 import SectionOne from './components/sectionOne'
 import SectionTwo from'./components/sectionTwo'
+import SectionThree from './components/sectionThree'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
    <Navbar/>
   <SectionOne/>
   <SectionTwo />
+  <SectionThree/>
 </>
   );
 }

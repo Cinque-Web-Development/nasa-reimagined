c50573d20c621af0484844fbaebf6db8dba8fe76
erikacolette <erikatidwell.c@gmail.com>
2021-05-04 18:03:12 -0400

    jupiter colors change

--- src/components/sectionTwo.js
+++ src/components/sectionTwo.js
@@ -6,12 +6,12 @@ import APOD from "./APOD/APOD";
 const SectionTwo = () => {
   return (
     <div className="sectiontwo-background">
-        <div className="jupiter-box">
-            <div className="jupitertop"></div>
-            <div className="jupiterRing"></div>
-            <div className="jupiterBottom"></div>
-        </div>
-       
+      <div className="jupiter-box">
+        <div className="jupitertop"></div>
+        <div className="jupiterRing"></div>
+        <div className="jupiterBottom"></div>
+      </div>
+
       <div className="news-feed">News Feed</div>
       <APOD />
     </div>
